a5aac780f018d903fb67a720fa6d48d3c965355d
Jenna Tripoli <jennamt1282@gmail.com>
2023-07-10 22:27:59 -0700

    Add ggpredict plot and analysis

--- app.R
+++ app.R
@@ -2,6 +2,8 @@ require(shiny)
 library(splitstackshape)
 library(pROC)
 library(nnet)
+library(ggeffects)
+library(corrplot)
 
 setup <- function() {
   sleepData <<- read.csv("sleepData.csv")
@@ -41,12 +43,6 @@ convert_numerical <- function() {
   sleepDataNum$BMI.Category[sleepDataNum$BMI.Category == "Overweight"] <<- 1
   sleepDataNum$BMI.Category[sleepDataNum$BMI.Category == "Obese"] <<- 2
   sleepDataNum$BMI.Category <<- as.numeric(sleepDataNum$BMI.Category)
-  
-  # convert sleep disorder to numeric values
-  sleepDataNum$Sleep.Disorder[sleepDataNum$Sleep.Disorder == "None"] <<- 0
-  sleepDataNum$Sleep.Disorder[sleepDataNum$Sleep.Disorder == "Insomnia"] <<- 1
-  sleepDataNum$Sleep.Disorder[sleepDataNum$Sleep.Disorder == "Sleep Apnea"] <<- 2
-  sleepDataNum$Sleep.Disorder <<- as.numeric(sleepDataNum$Sleep.Disorder)
 }
 
 # split the sleepData and sleepDataNum data frames in half randomly
@@ -61,6 +57,13 @@ split_data <- function() {
   row.names(testData) <- NULL
   row.names(trainDataNum) <- NULL
   row.names(testDataNum) <- NULL
+
+  # convert sleep disorder to numeric values
+  trainDataNum2 <<- trainDataNum
+  trainDataNum2$Sleep.Disorder[trainDataNum2$Sleep.Disorder == "None"] <<- 0
+  trainDataNum2$Sleep.Disorder[trainDataNum2$Sleep.Disorder == "Insomnia"] <<- 1
+  trainDataNum2$Sleep.Disorder[trainDataNum2$Sleep.Disorder == "Sleep Apnea"] <<- 2
+  trainDataNum2$Sleep.Disorder <<- as.numeric(trainDataNum2$Sleep.Disorder)
 }
 
 model_predict <- function(model, data, column) {
@@ -85,6 +88,7 @@ make_model <- function(features) {
   # use model to predict values for training and testing data
   trainDataNum <<- model_predict(model, trainDataNum, "Predicted")
   testDataNum <<- model_predict(model, testDataNum, "Predicted")
+  return (model)
 }
 
 model_accuracy <- function(features, data, column, type) {
@@ -110,9 +114,17 @@ plot_roc <- function(features, roc) {
   
   # create plot for roc curve
   plot <- plot.roc(roc[['rocs']][[1]], main="ROC Curve for Current Model")
-  sapply(2:length(roc[['rocs']]), function(i) lines.roc(roc[['rocs']][[i]], col=i))
+  sapply(2:length(roc[['rocs']]), function(i) { lines.roc(roc[['rocs']][[i]], col=i) })
   legend("bottomright", c("None", "Insomnia", "Sleep Apnea"), fill=c("black", "red", "green"))
   return (plot)
+}
+
+plot_regression <- function(features, model) {
+  # require that at least 1 feature is selected
+  req(length(features) > 0)
+  
+  # create plot for logistic regression model
+  return (plot(ggpredict(model, se=TRUE, interactive=TRUE)))
 }
 
 setup()
@@ -136,8 +148,10 @@ ui <- fluidPage(
     mainPanel(
       tabsetPanel(type = "tabs",
                   tabPanel("Description", uiOutput("description")),
-                  tabPanel("ROC Plot", plotOutput("rocPlot")),
-                  tabPanel("Analysis", uiOutput("analysis"))
+                  tabPanel("Regression Plot", uiOutput("lineBreak2"), plotOutput("regressionPlot")),
+                  tabPanel("ROC Plot", uiOutput("lineBreak3"), plotOutput("rocPlot")),
+                  tabPanel("Mathematics", uiOutput("mathematics1"), uiOutput("formula1"), uiOutput("mathematics2"), uiOutput("formula2"), uiOutput("mathematics3")),
+                  tabPanel("Analysis", uiOutput("analysis1"), plotOutput("corrPlot"), uiOutput("analysis2"))
       )
     )
   )
@@ -145,44 +159,106 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   display_data <- function() {
-    make_model(input$features)
-    output$rocPlot <- renderPlot ({ plot_roc(input$features, make_roc(input$features)) })
-    output$auc0 <- renderText({ toString(paste("AUC for None:", round(auc(make_roc(input$features)[['rocs']][[1]]), 2))) })
-    output$auc1 <- renderText({ toString(paste("AUC for Insomnia:", round(auc(make_roc(input$features)[['rocs']][[2]]), 2))) })
-    output$auc2 <- renderText({ toString(paste("AUC for Sleep Apnea:", round(auc(make_roc(input$features)[['rocs']][[3]]), 2))) })
-    output$auc3 <- renderText({ toString(paste("<b>Overall AUC:", round(auc(make_roc(input$features)), 2), "</b>")) })
+    if (length(input$features) > 0) {
+      output$auc0 <- renderText({ toString(paste("AUC for None:", round(auc(roc[['rocs']][[1]]), 2))) })
+      output$auc1 <- renderText({ toString(paste("AUC for Insomnia:", round(auc(roc[['rocs']][[2]]), 2))) })
+      output$auc2 <- renderText({ toString(paste("AUC for Sleep Apnea:", round(auc(roc[['rocs']][[3]]), 2))) })
+      output$auc3 <- renderText({ toString(paste("<b>Overall AUC:", round(auc(roc), 2), "</b>")) })
+    } else {
+      output$auc0 <- renderText({ toString(paste("")) })
+      output$auc1 <- renderText({ toString(paste("")) })
+      output$auc2 <- renderText({ toString(paste("")) })
+      output$auc3 <- renderText({ toString(paste("<b>Please select at least 1 feature.</b>")) })
+    }
+    
+    model <- make_model(input$features)
+    roc <- make_roc(input$features)
+    output$regressionPlot <- renderPlot ({ suppressMessages(plot_regression(input$features, model)) })
+    output$rocPlot <- renderPlot ({ plot_roc(input$features, roc) })
   }
   
   # create plot with selected features
-  observeEvent(input$features, { display_data() })
+  observeEvent(input$features, { display_data() }, ignoreNULL=FALSE)
   
   # randomize data split and create plot
   observeEvent(input$splitData, {
     split_data()
     display_data()
+    output$corrPlot <- renderPlot({ corrplot(cor(trainDataNum2[, -1]), tl.col="black", tl.cex=1) })
   })
   
   output$lineBreak1 <- renderUI({ HTML("<br>") })
+  output$lineBreak2 <- renderUI({ HTML("<br>") })
+  output$lineBreak3 <- renderUI({ HTML("<br>") })
 
   output$description <- renderUI({
     HTML("<br> In this case study, we will look at a data set about sleep health and lifestyle for 374 participants.
     The data set includes features such as gender, age, occupation, sleep duration, quality of sleep, physical activity level,
-    stress level, BMI category, blood pressure, heart rate, daily steps, and what sleep disorder they have (if applicable).
+    stress level, BMI category, blood pressure, heart rate, daily steps, and what sleep disorder they have (if applicable). The 
+    original data set can be found on Kaggle <a href='https://www.kaggle.com/datasets/uom190346a/sleep-health-and-lifestyle-dataset'>here</a>.
     The purpose of this case study is to find what features are useful in predicting if a patient has a sleeping disorder and
-    creating a classification algorithm to accomplish this with a testing data set. <br><br>
+    creating a classification algorithm to accomplish this with a testing data set. The motivation for researching this topic is that 
+    it is about a very relevant subject, since many people suffer from sleep issues. When looking for a data set, it was important to 
+    me to find one that was about a topic that I find personal interest in and had a variety of different features to look at. <br><br>
     
-    In this application, a multinomial classification model was developed to predict whether a participant has a sleeping disorder.
-    The plot are the ROC curves, which shows the overall performance of the classification model for each class (none, insomnia, and 
-    sleep apnea). The check boxes in the side panel allows for selecting desired features to use for the model. To develop a 
-    classification model, the data set must be split into a training set and testing set. The button in the side panel will randomly 
-    split the original data set in half to create these two data sets. <br><br>")
-  })
+    In this application, a multinomial classification model was developed to predict whether a participant has a sleeping disorder. The
+    regresssion plot displays the distribution of (p) and (1-p) for each class (none, insomnia, and sleep apnea).The ROC plot shows the 
+    overall performance of the classification model for each class. The check boxes in the side panel allows for selecting desired features 
+    to use for the model. To develop a classification model, the data set must be split into a training set and testing set. The button in 
+    the side panel will randomly split the original data set in half to create these two data sets.") })
   
-  output$analysis <- renderUI({
-    HTML("<br> select an algorithm suitable for the data set (classification/regression/clustering/other) <br><br>
-         explain the mathematical/statistical details of the algorithm <br><br>
-         see how changing parameters effects the results. explain the data set and machine learning modeling methodology <br><br>")
-  })
+  output$mathematics1 <- renderUI({ 
+    HTML("<br>Visualizing a logistic regression model involves plotting the overall fraction of the response variable, with probability 
+         on the y-axis. The model returns a score that estimates the probability for each of the three classes for sleep disorder.
+         There is one plot for each discrete option for the value of sleep disorder since this is a multinomial model. Each graph 
+         compares the distribution of participants with that specific value for sleep disorder and those who do not as a function of 
+         the model's predicted probability.
+         
+         We can use an ROC curve as a visual diagnostic tool for the logistic regression model. The area under the curve represents how
+         well the model is able to predict, with the ideal value being AUC = 1. On an ROC curve, the x-axis is the False Positive Rate
+         and the y-axis is the True Positive Rate. When AUC = 1, the curve of the graph is to the upper left corner, where sensitivity = 1 
+         and specificity = 0. This means that the closer the ROC curve is to the upper left corner, the higher the accuracy of the model is.
+         
+         Below is the formula for logistic regression:") })
+  
+  output$formula1 <- renderUI({ withMathJax(helpText("$$ln(\\frac{p}{1-p})=b_{0}+b_{1}x$$")) })
+  output$mathematics2 <- renderUI({ HTML("For modeling purposes, this formula is rearranged to become the following formula: <br><br>") })
+  output$formula2 <- renderUI({ withMathJax(helpText("$$p=\\frac{1}{1+e^{-(b_{0}+b_{1}x)}}$$")) })
+  
+  output$mathematics3 <- renderUI({ 
+    HTML("Plotting logistic regression has an S-curve, with the predicted y-values lying within the range of 0 to 1. The case of 
+         interest is y = 1 because this indicates 'true'. Logit(P(y = 1)) is inverted by the sigmoid function to compute label
+         probabililities. Categorical variables are expanded with one-hot encoding. Logistic regression is an iterative solution,
+         since the least squares are re-weighted over and over until the optimized model is found.") })
+
+  output$analysis1 <- renderUI({
+    HTML("<br> Sleep disorder is a categorical variable since there are three discrete values it can have -- 'None', 'Insomnia', and
+         'Sleep Apnea'. The algorithm we want to use should be for supervised learning because we are making a predictive model.
+         Knowing that we are predicting a categorical variable with supervised learning, there are a variety of options for algorithms
+         to use. I selected logistic regression because it represents the relationship between a single dependent discrete variable 
+         (response) and one or more independent variables (predictors). Additionally, logistic regression is especially useful in
+         this case because of there being multiple classes to predict. The features being input were both numerical and categorical,
+         and logistic regression can handle this well with classification.
+         
+         Through testing out different combinations of features, we can find a good group of predictors to use for the model. To have 
+         some guidance of what features to look at, I created a correlation matrix for all available features in the training data. <br><br>") })
+  
+  output$corrPlot <- renderPlot({ corrplot(cor(trainDataNum2[, -1]), tl.col="black", tl.cex=1) })
+  
+  output$analysis2 <- renderUI({
+    HTML("<br> From the correlation matrix, we can see that BMI category, systolic blood pressure, and diastolic blood pressure all have strong 
+         positive correlations with sleep disorder. Age and occupation also have moderate positive correlations with sleep disorder. 
+         Therefore, these are the variables that we will test out first with our model. The variables sleep duration, quality of sleep, 
+         stress level, and daily steps all have very weak correlations with sleep disorder, so they will likely not be considered for 
+         our optimal model and are noisy data. <br><br>
+         
+         When looking at the ROC curve for a model with predictors of the three features with strong positive correlations, we can observe 
+         that the overall AUC is usually above 0.8, which is a good sign. However, we can see some decent variability in the overall AUC 
+         when re-splitting the data to change the training set. This is an indication to add additional features to the model, so I decided 
+         to include age and occupation as well. This reduced the variability in the overall AUC and made it consistently above 0.82, making 
+         this model the optimal choice. To prove the point about the other available features being noisy data, we can also test out a model 
+         with all 12 features selected as predictors. In doing this, we can see that the overall AUC has very similar behavior to that of the 
+         previous model with 5 features, showing that the other 7 variables are not necessary when predicting sleep disorder.") })
 }
 
 shinyApp(ui=ui, server=server)
